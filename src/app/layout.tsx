fd60a65774c89aaadfade8807eabb20ac47ce7dd
Cristian Peschke <cpschk.cl@gmail.com>
2025-07-21 16:37:59 +0000

    sigue apareciendo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body bg-background text-foreground antialiased">
+      <body className="font-body bg-background text-foreground antialiased overflow-x-hidden">
         {children}
         <Toaster />
       </body>
