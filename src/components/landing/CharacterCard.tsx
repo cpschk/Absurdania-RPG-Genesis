9178df525be42911fff5822260bf10c913ddb07e
Cristian Peschke <cpschk.cl@gmail.com>
2025-07-22 15:16:49 +0000

    podemos hacer ahora responsivo el escalado de la imagen del personaje de

--- src/components/landing/CharacterCard.tsx
+++ src/components/landing/CharacterCard.tsx
@@ -113,7 +113,7 @@ export function CharacterCard({
                     alt={name}
                     fill
                     className={cn(
-                      "object-cover mx-auto scale-75",
+                      "object-cover mx-auto scale-90 md:scale-75",
                       getImageGlowClass(type)
                     )}
                     data-ai-hint={dataAiHint}
